906d8ca8c9008c957cda0268de38ead8ae5d7007
TautvydasGec <gecevicius.tautvydas@gmail.com>
2020-03-29 14:58:45 +0000

    Added project files

---
+++ src/car.cc
@@ -0,0 +1,354 @@
+/* Copyright (c) 2019 Francis James Franklin
+ * 
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided
+ * that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and
+ *    the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ *    the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <signal.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include "Client.hh"
+#include "Serial.hh"
+
+#define CARIOT_WEBDIR "/home/pi/cariot/www/"
+
+static void loglist();
+
+class Car : public Client, public Serial::Command
+{
+private:
+  Serial m_S;
+
+  float x_actual;
+  float y_actual;
+  float slip_l;
+  float slip_r;
+
+  const char *m_pattern;
+
+  int m_length;
+
+public:
+  Car(const char *serial, bool verbose, bool fixbaud) : Client("car", verbose),
+                                                        m_S(this, serial, fixbaud, verbose),
+                                                        x_actual(0),
+                                                        y_actual(0)
+  {
+    set_sleeper(&m_S);
+
+    m_pattern = "/cariot/#";
+    m_length = strlen(m_pattern) - 1;
+  }
+  virtual ~Car()
+  {
+    // ...
+  }
+  virtual void serial_connect()
+  {
+    fprintf(stdout, "car: connected to Arduino\n");
+  }
+  virtual void serial_disconnect()
+  {
+    fprintf(stdout, "car: disconnected from Arduino\n");
+  }
+  virtual void serial_command(char command, unsigned long value)
+  {
+    switch (command)
+    {
+    case 'x':
+    {
+      x_actual = (-127 + (float)value) / 127;
+    }
+    break;
+    case 'y':
+    {
+      y_actual = (-127 + (float)value) / 127;
+    }
+    break;
+    case 'l':
+    {
+    }
+      virtual void tick()
+      { // every millisecond
+        static unsigned count = 0;
+        static char buffer[32];
+
+        if (++count == 100)
+        {
+          count = 0;
+          sprintf(buffer, "%.3f %.3f", x_actual, y_actual);
+          publish("/cariot/car/XY", buffer);
+        }
+
+        Client::tick(); // network update
+      }
+      virtual void second()
+      { // every second
+        if (!m_S.connected())
+        {
+          m_S.connect();
+        }
+        Client::second();
+      }
+      virtual void setup()
+      {
+        if (!subscribe(m_pattern))
+        {
+          if (verbose())
+            fprintf(stdout, "car: Failed to subscribe with pattern %s\n", m_pattern);
+        }
+      }
+      virtual void message(const char *topic, const char *message, int length)
+      {
+        static char buf[32];
+
+        if (length > 31)
+        {
+          length = 31;
+        }
+        strncpy(buf, message, length);
+        buf[length] = 0;
+
+        topic += m_length;
+
+        if (verbose())
+          fprintf(stdout, "car: message=\"%s\" @ %s\n", buf, topic);
+
+        if (strcmp(topic, "system/exit") == 0)
+        {
+          if (strcmp(message, "car") == 0)
+          {
+            stop();
+          }
+          else if (strcmp(message, "controller") == 0)
+          {
+            // Do something to shutdown the Raspberry Pi??
+          }
+        }
+        else if (strcmp(topic, "dash/XY") == 0)
+        {
+          float x, y;
+          if (sscanf(message, "%f %f", &x, &y) == 2)
+          {
+            int ix = (int)((1 + x) * 127);
+            int iy = (int)((1 + y) * 127);
+            ix = (ix < 0) ? 0 : ((ix > 254) ? 254 : ix);
+            iy = (iy < 0) ? 0 : ((iy > 254) ? 254 : iy);
+            m_S.write('x', (unsigned long)ix);
+            m_S.write('y', (unsigned long)iy);
+          }
+        }
+      }
+    };
+
+    class Logger : public Ticker, public Serial::Report
+    {
+    private:
+      int m_log; // must be initialised before m_S
+
+      Serial m_S;
+
+    public:
+      Logger(const char *serial, bool verbose, bool fixbaud) : m_log(-1),
+                                                               m_S(this, serial, fixbaud, verbose)
+      {
+        set_sleeper(&m_S);
+      }
+      virtual ~Logger()
+      {
+        // ...
+      }
+      virtual void serial_connect()
+      {
+        fprintf(stderr, "logger: connected to Arduino\n");
+
+        char logx[] = CARIOT_WEBDIR "logs/log-XXXXXX.csv";
+
+        m_log = mkstemps(logx, 4);
+        if (m_log != -1)
+        {
+          fprintf(stderr, "logger [%d]: log file created\n", m_log);
+        }
+      }
+      virtual void serial_disconnect()
+      {
+        fprintf(stderr, "logger [%d]: disconnected from Arduino\n", m_log);
+
+        if (m_log != -1)
+        {
+          close(m_log);
+          m_log = -1;
+          fprintf(stderr, "logger: log file closed\n");
+        }
+      }
+      virtual void serial_report(const char *report)
+      {
+        if (m_log != -1)
+        {
+          int length = strlen(report);
+          write(m_log, report, length);
+        }
+      }
+      virtual void tick()
+      { // every millisecond
+        Ticker::tick();
+      }
+      virtual void second()
+      { // every second
+        if (!m_S.connected())
+        {
+          m_S.connect();
+        }
+      }
+    };
+
+    int main(int argc, char **argv)
+    {
+      const char *serial = "/dev/ttyACM0";
+
+      bool verbose = false;
+      bool fixbaud = false;
+      bool logger = false;
+
+      for (int arg = 1; arg < argc; arg++)
+      {
+        if (strcmp(argv[arg], "--help") == 0)
+        {
+          fprintf(stderr, "\n%s [--help] [--verbose] [/dev/<ID>]\n\n", argv[0]);
+          fprintf(stderr, "  --help     Display this help.\n");
+          fprintf(stderr, "  --verbose  Print debugging info.\n");
+          fprintf(stderr, "  --fix-baud Fix the BAUD rate as 115200.\n");
+          fprintf(stderr, "  --logger   Run as a data logger.\n");
+          fprintf(stderr, "  /dev/<ID>  Connect to /dev/<ID> instead of default [/dev/ttyACM0].\n\n");
+          return 0;
+        }
+        if (strcmp(argv[arg], "--verbose") == 0)
+        {
+          verbose = true;
+        }
+        else if (strcmp(argv[arg], "--fix-baud") == 0)
+        {
+          fixbaud = true;
+        }
+        else if (strcmp(argv[arg], "--logger") == 0)
+        {
+          logger = true;
+        }
+        else if (strncmp(argv[arg], "/dev/", 5) == 0)
+        {
+          serial = argv[arg];
+        }
+        else
+        {
+          fprintf(stderr, "%s [--help] [--verbose] [--logger] [--fix-baud] [/dev/ID]\n", argv[0]);
+          return -1;
+        }
+      }
+
+      if (logger)
+      {
+        if (!fork())
+        {
+          loglist();
+          exit(0);
+        }
+        Logger(serial, verbose, fixbaud).loop();
+      }
+      else
+      {
+        Car(serial, verbose, fixbaud).loop();
+      }
+      return 0;
+    }
+
+    static const char *size_string(off_t bytes)
+    {
+      static char buffer[16];
+      if (bytes < 1024)
+      {
+        snprintf(buffer, 16, "%14lu ", (unsigned long)bytes);
+      }
+      else
+      {
+        float k = (float)bytes / 1024.0;
+        snprintf(buffer, 16, "%14.2fk", k);
+      }
+      return buffer;
+    }
+
+    static const char *time_string(const time_t *t_mod)
+    {
+      static char buffer[32];
+      struct tm *info = localtime(t_mod);
+      strftime(buffer, 31, "%c", info);
+      return buffer;
+    }
+
+    void loglist()
+    {
+      const char *logdir = CARIOT_WEBDIR "logs";
+      const char *loglist = CARIOT_WEBDIR "logs.html";
+
+      if (chdir(logdir) < 0)
+        return;
+
+      FILE *f = fopen(loglist, "w");
+      if (f)
+      {
+        fputs("<html>\n <head>\n  <title>Cariot Log File Listing</title>\n  <style type=\"text/css\">\n", f);
+        fputs("li {\n\tfont-family: Lucida Console, Courier, monospace;\n\twhite-space: pre;\n}\n", f);
+        fputs("  </style>\n </head>\n <body>\n  <ul>\n", f);
+
+        DIR *d = opendir(".");
+        if (d)
+        {
+          while (true)
+          {
+            struct dirent *e = readdir(d);
+            if (!e)
+              break;
+
+            if (e->d_type == DT_REG) // it's a regular file
+              if (strcmp(e->d_name + strlen(e->d_name) - 4, ".csv") == 0)
+              { // with a .csv suffix
+                struct stat s;
+                if (!stat(e->d_name, &s))
+                {
+                  const char *file_size = size_string(s.st_size);
+                  const char *file_time = time_string(&s.st_mtime);
+
+                  fprintf(f, "   <li><a href=\"logs/%s\">%s %s  %s</a></li>\n", e->d_name, file_time, file_size, e->d_name);
+                }
+              }
+          }
+          closedir(d);
+        }
+        fputs("  </ul>\n </body>\n</html>\n", f);
+        fclose(f);
+      }
+    }
---
+++ www/dash.js
@@ -0,0 +1,404 @@
+var client;
+
+const navBar = document.getElementById('navbar');
+const navItem = document.getElementsByClassName('nav-item');
+const coordButton = document.getElementById('coordButton');
+const zone = document.getElementById('zone');
+const zone2 = document.getElementById('zone2');
+const circle = document.getElementById('circle');
+const zoneCircle = document.getElementById('zoneCircle');
+const values = document.getElementById('values');
+const directions = document.getElementById('directions');
+const explanation = document.getElementById('explanation');
+const modeButton = document.getElementById('modeButton');
+const controllerButton = document.getElementById('controllerButton');
+const buttons = document.getElementById('button-container');
+let width =
+  window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth;
+let height =
+  window.innerHeight ||
+  document.documentElement.clientHeight ||
+  document.body.clientHeight;
+let coords = 1;
+
+window.onload = joyStickControll;
+
+function coordsButton() {
+  if (coords === 1) {
+    values.style.display = 'none';
+    coordButton.style.color = 'red';
+    coords = 0;
+  } else {
+    values.style.display = 'block';
+    coordButton.style.color = '#b6b6b6';
+    coords = 1;
+  }
+}
+
+let mode = 1;
+function modesButton() {
+  /* light mode */
+  if (mode === 1) {
+    zone.style.backgroundColor = 'rgba(235, 235, 235, 1)';
+    values.style.color = 'black';
+    explanation.style.color = 'black';
+    circle.style.background = 'rgba(71, 71, 71, 0.9)';
+    zoneCircle.style.background = 'rgba(71, 71, 71, 0.9)';
+    modeButton.style.color = 'yellow';
+    mode = 0;
+  } else {
+    /* dark mode */
+    zone.style.backgroundColor = 'rgb(54, 54, 54)';
+    values.style.color = 'white';
+    explanation.style.color = 'white';
+    circle.style.background = 'rgba(180, 180, 180, 0.9)';
+    zoneCircle.style.background = 'rgba(180, 180, 180, 0.9)';
+    modeButton.style.color = '#b6b6b6';
+    mode = 1;
+  }
+}
+
+let controller = 1;
+joyStickControll();
+function controllersButton() {
+  if (controller === 1) {
+    controllerButton.style.color = 'red';
+    controller = 0;
+    joyStickControll();
+  } else {
+    controllerButton.style.color = '#b6b6b6';
+    controller = 1;
+    joyStickControll();
+  }
+}
+
+function moveup() {
+  directions.textContent = 'UP';
+}
+
+function moveleft() {
+  directions.textContent = 'LEFT';
+}
+
+function moveright() {
+  directions.textContent = 'RIGHT';
+}
+
+function movedown() {
+  directions.textContent = 'DOWN';
+}
+
+/* MQTT SEND */
+function mqtt_send_XY(distx, disty) {
+  message = new Paho.Message(distx.toFixed(3) + ' ' + disty.toFixed(3));
+  message.destinationName = '/cariot/dash/XY';
+  client.send(message);
+}
+
+/////////////////////////////////////////////////
+//////////////////* main controller *///////////
+///////////////////////////////////////////////
+function joyStickControll() {
+  if (controller === 1) {
+    explanation.style.display = 'block';
+    zone2.style.display = 'none';
+    buttons.style.display = 'none';
+
+    let on = document.addEventListener.bind(document);
+    let xmouse, ymouse, xStaticMouse, yStaticMouse;
+    let xtouch, ytouch, xStaticTouch, yStaticTouch;
+    let dist = 0,
+      distx = 0,
+      disty = 0,
+      barrierDist = 75;
+
+    let destroy = false;
+
+    on('mousemove', function(e) {
+      xmouse = e.clientX || e.pageX;
+      ymouse = e.clientY || e.pageY;
+    });
+    on('mousedown', function(e) {
+      xStaticMouse = e.clientX || e.pageX;
+      yStaticMouse = e.clientY || e.pageY;
+    });
+
+    on('touchmove', function(e) {
+      xtouch = e.touches[0].clientX || e.pageX;
+      ytouch = e.touches[0].clientY || e.pageY;
+    });
+    on('touchstart', function(e) {
+      xStaticTouch = e.touches[0].clientX || e.pageX;
+      yStaticTouch = e.touches[0].clientY || e.pageY;
+      xtouch = xStaticTouch;
+      ytouch = yStaticTouch;
+    });
+
+    let x = void 0,
+      y = void 0,
+      dx = void 0,
+      dy = void 0,
+      tx = 0,
+      ty = 0,
+      key = -1,
+      xs = void 0,
+      ys = void 0;
+
+    let followMouse = function followMouse() {
+      key = requestAnimationFrame(followMouse);
+      xs = xStaticMouse;
+      ys = yStaticMouse;
+      if (!destroy) {
+        if (x || !y) {
+          x = xmouse;
+          y = ymouse;
+        } else {
+          dx = (xmouse - x) * 0.125;
+          dy = (ymouse - y) * 0.125;
+          if (Math.abs(dx) + Math.abs(dy) < 0.1) {
+            x = xmouse;
+            y = ymouse;
+          } else {
+            x += dx;
+            y += dy;
+          }
+        }
+
+        /*      barrier fo x movement*/
+        if (x < xs - barrierDist) {
+          x = xs - barrierDist;
+        } else if (x > xs + barrierDist) {
+          x = xs + barrierDist;
+        } else {
+          x = x;
+        }
+        /*      barrier for y movement*/
+        if (y < ys - barrierDist) {
+          y = ys - barrierDist;
+        } else if (y > ys + barrierDist) {
+          y = ys + barrierDist;
+        } else {
+          y = y;
+        }
+        /*      animation*/
+        circle.style.left = x + 'px';
+        circle.style.top = y + 'px';
+        zoneCircle.style.left = xStaticMouse + 'px';
+        zoneCircle.style.top = yStaticMouse + 'px';
+        if (yStaticMouse > height - 170) {
+          values.style.marginTop = -90 + 'px';
+        } else {
+          values.style.marginTop = 200 + 'px';
+        }
+        if (xStaticMouse < 200) {
+          values.style.marginLeft = 100 + 'px';
+        } else {
+          values.style.marginLeft = 0 + 'px';
+        }
+        /*      distance calculation*/
+        dist = Math.sqrt((x - xs) ** 2 + (y - ys) ** 2);
+        distx = x - xs;
+        disty = ys - y;
+      } else {
+        //when mouse is not pressed sending 0
+        dist = 0;
+        distx = 0;
+        disty = 0;
+      }
+      document.getElementById('x-value').textContent = distx;
+      document.getElementById('y-value').textContent = disty;
+      mqtt_send_XY(distx, disty);
+    };
+
+    /* mouse */
+    function generateJoyStick(event, bMouse, type) {
+      if (bMouse) {
+        zoneCircle.style.display = 'block';
+        circle.style.display = 'block';
+        destroy = false;
+        /* Changing the values position to not hide under screen */
+        followMouse();
+      }
+    }
+    function destroyJoyStick(event, bMouse, type) {
+      if (bMouse) {
+        circle.style.display = 'none';
+        zoneCircle.style.display = 'none';
+        destroy = true;
+      }
+    }
+    function e_m_down(event) {
+      generateJoyStick(event, true, 'down');
+    }
+    function e_m_up(event) {
+      destroyJoyStick(event, true, 'up');
+      generateJoyStick(event, false, 'down');
+    }
+    function e_m_out(event) {
+      destroyJoyStick(event, true, 'up');
+      generateJoyStick(event, false, 'down');
+    }
+
+    ////////////////////////////////////////////////////////
+    ////////////////////followTouch/////////////////////////
+    /////////////////////////////////////////////////////
+    let followTouch = function followTouch() {
+      key = requestAnimationFrame(followTouch);
+      xs = xStaticTouch;
+      ys = yStaticTouch;
+      if (!destroy) {
+        if (x || !y) {
+          x = xtouch;
+          y = ytouch;
+        } else {
+          dx = (xtouch - x) * 0.125;
+          dy = (ytouch - y) * 0.125;
+          if (Math.abs(dx) + Math.abs(dy) < 0.1) {
+            x = xtouch;
+            y = ytouch;
+          } else {
+            x += dx;
+            y += dy;
+          }
+        }
+
+        /*      barrier fo x movement*/
+        if (x < xs - barrierDist) {
+          x = xs - barrierDist;
+        } else if (x > xs + barrierDist) {
+          x = xs + barrierDist;
+        } else {
+          x = x;
+        }
+        /*      barrier for y movement*/
+        if (y < ys - barrierDist) {
+          y = ys - barrierDist;
+        } else if (y > ys + barrierDist) {
+          y = ys + barrierDist;
+        } else {
+          y = y;
+        }
+        /*      animation*/
+        circle.style.left = x + 'px';
+        circle.style.top = y + 'px';
+
+        zoneCircle.style.left = xStaticTouch + 'px';
+        zoneCircle.style.top = yStaticTouch + 'px';
+
+        /* values */
+        if (yStaticTouch > height - 250) {
+          values.style.marginTop = -90 + 'px';
+        } else {
+          values.style.marginTop = 200 + 'px';
+        }
+        if (xStaticTouch < 90) {
+          values.style.marginLeft = 100 + 'px';
+        } else {
+          values.style.marginLeft = 0 + 'px';
+        }
+        /*      distance calculation*/
+        dist = Math.sqrt((x - xs) ** 2 + (y - ys) ** 2);
+        distx = x - xs;
+        disty = ys - y;
+      } else {
+        //when Touch is not pressed sending 0
+        dist = 0;
+        distx = 0;
+        disty = 0;
+      }
+      document.getElementById('x-value').textContent = distx;
+      document.getElementById('y-value').textContent = disty;
+      mqtt_send_XY(distx, disty);
+    };
+
+    /* Touch */
+    function generateJoyStickT(event, bTouch, type) {
+      if (bTouch) {
+        zoneCircle.style.display = 'block';
+        circle.style.display = 'block';
+        destroy = false;
+        followTouch();
+      }
+    }
+    function destroyJoyStickT(event, bTouch, type) {
+      if (bTouch) {
+        circle.style.display = 'none';
+        zoneCircle.style.display = 'none';
+        destroy = true;
+      }
+    }
+    function e_t_start(event) {
+      generateJoyStickT(event, true, 'start');
+    }
+    function e_t_end(event) {
+      destroyJoyStickT(event, true, 'end');
+      generateJoyStickT(event, false, 'start');
+    }
+    function e_t_cancel(event) {
+      destroyJoyStickT(event, true, 'end');
+      generateJoyStickT(event, false, 'start');
+    }
+
+    /* EVENT Listeners */
+
+    if ('ontouchstart' in document.documentElement) {
+      zone.addEventListener('touchstart', e_t_start, false);
+      zone.addEventListener('touchcancel', e_t_cancel, false);
+      zone.addEventListener('touchend', e_t_end, false);
+      //zone.addEventListener("touchmove", e_t_move, false);
+    } else {
+      zone.addEventListener('mousedown', e_m_down, false);
+      zone.addEventListener('mouseup', e_m_up, false);
+      //zone.addEventListener("mousemove", e_m_move, false);
+      zone.addEventListener('mouseleave', e_m_out, false);
+    }
+  } else {
+    buttons.style.display = 'block';
+    explanation.style.display = 'none';
+    zone2.style.display = 'block';
+  }
+}
+
+/* MQTT callbacks
+ */
+
+// called when the client connects
+function onConnect() {
+  // Once a connection has been made, make a subscription and send a message.
+  mqtt_log_update('onConnect');
+
+  client.subscribe('/cariot/car/#');
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    mqtt_log_update('onConnectionLost:' + responseObject.errorMessage);
+  }
+}
+
+function get_started() {
+  var mosquitto_host = window.location.hostname;
+  var mosquitto_port = 80; // window.location.port;
+
+  var val_host = document.getElementById('val_host');
+  var val_port = document.getElementById('val_port');
+
+  val_host.innerHTML = mosquitto_host;
+  val_port.innerHTML = mosquitto_port.toString();
+
+  // Create a client instance
+  var client_id = 'web-dash-' + Math.random().toString();
+  client = new Paho.Client(mosquitto_host, Number(mosquitto_port), client_id);
+
+  // set callback handlers
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
+
+  // connect the client
+  client.connect({ onSuccess: onConnect });
+}
+
+window.onload = get_started;
